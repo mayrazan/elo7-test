b20de0e2e54327008b37d9499b5e80c9494bf05c
mayrazan <mayramanchein@yahoo.com.br>
2020-10-22 18:56:05 -0300

    Finishing page and organizing style elements

--- frontend/script/vagas.js
+++ frontend/script/vagas.js
@@ -2,14 +2,17 @@
 	el: '#app',
 	data() {
 		return {
+			//local onde serão armazenadas as informações da API
 			info: [],
 			localizacao: null
 		}
 	},
 	methods: {
+		//retorna as vagas ativas
 		ativa: function(lista){
 			return lista.filter(vaga => vaga.ativa);
 		},
+		//retorna a localização como remota se não possuir nada
 		local: function(lista) {
 			const a = this.ativa(lista);
 			const l = "Remoto";
@@ -20,12 +23,14 @@
 			return a;
 		}
 	},
+	//pega as informações da API utilizando o axios já com os dados tratados
 	mounted() {
 		axios
 			.get('http://www.mocky.io/v2/5d6fb6b1310000f89166087b')
 			.then(response => (this.info = this.local(response.data.vagas)))
 	},
 	created(){
+		//aponta para as instâncias criadas
 		this.info;
 		this.localizacao;
 	}
